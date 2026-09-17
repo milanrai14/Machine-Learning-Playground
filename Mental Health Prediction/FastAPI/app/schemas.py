1cb0f89cf086f47db6e4edd1d6c6a8e50f1293c7
Milan Rai <milanraii014@gmail.com>
2026-09-17 09:31:24 +0545

    Schemas of HealtDetailRequest

--- Mental Health Prediction/FastAPI/app/schemas.py
+++ Mental Health Prediction/FastAPI/app/schemas.py
@@ -8,3 +8,14 @@ class HealthDetailRequest(BaseModel):
     Academic_Level : Literal['Undergraduate', 'Graduate', 'High School']
     Most_Used_Platform: str 
     Purpose_Of_Use: Literal['Networking', 'Education', 'Entertainment']
+    Avg_Daily_Usage_Hours: float
+    Daily_Unlocks: int 
+    Study_Hours: float
+    Physical_Activity_Hours: float
+    Sleep_Hours_Per_Night: float
+    Stress_Level: str
+    
+
+    
+
+
