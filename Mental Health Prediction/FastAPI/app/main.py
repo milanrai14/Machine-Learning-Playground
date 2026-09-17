960f6d63f52ec6481fff3c1039bbcc533d3cbeb5
Milan Rai <milanraii014@gmail.com>
2026-09-17 09:50:46 +0545

    main.py

--- Mental Health Prediction/FastAPI/app/main.py
+++ Mental Health Prediction/FastAPI/app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI, HTTPException
+from schemas import HealthDetailRequest
+from prediction import predict_mental_healh
 
-app = FastAPI()
+app = FastAPI(
+    title="Mental Health Prediction API",
+    description="API for predicting mental health using Machine Learning",
+    version="1.0.0"
+)
 
 @app.get("/")
 def home():
@@ -8,3 +14,10 @@ def home():
         "Message": "Meantal Health Score Prediction Of the Student"
     }
 
+@app.post('/predict')
+def health_prediction(req: HealthDetailRequest):
+    prediction = predict_mental_healh(req)
+
+    return {
+        'Prediction': prediction
+    }
