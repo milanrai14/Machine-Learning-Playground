200d46ddac7049e27677cd69f97e754cbf2d8fa1
Milan Rai <milanraii014@gmail.com>
2026-09-17 09:47:44 +0545

    Prediction function

---
+++ Mental Health Prediction/FastAPI/app/prediction.py
@@ -0,0 +1,23 @@
+from model_loader import model
+import pandas as pd 
+
+def predict_mental_healh(req):
+    input_data = {
+        'Age': req.Age,
+        'Gender': req.Gender, 
+        'Country': req.Country, 
+        'Academic_Level': req.Academic_level, 
+        'Most_Used_Platform': req.Most_Used_Platform, 
+        'Purpose_Of_Use': req.Purpose_Of_Use, 
+        'Avg_Daily_Usage_Hours': req.Avg_Daily_Usage_Hours,
+        'Daily_Unlocks': req.Daily_Unlocks, 
+        'Study_Hours': req.Study_Hours, 
+        'Physical_Activity_Hours': req.Physical_Activity_Hours, 
+        'Sleep_Hours_per_Night': req.Sleep_Hours_Per_Night, 
+        'Stress_Level': req.Stress_Level
+    }
+
+    df = pd.DataFrame([input_data])
+
+    prediction = model.predict(df)
+    return prediction
